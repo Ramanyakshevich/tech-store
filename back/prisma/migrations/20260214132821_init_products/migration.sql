4518cc743b6dfae5e8b76ce2716b9df393e12463
RamanYakshevich <s227736@sggw.edu.pl>
2026-02-14 14:37:39 +0100

    connected DB

--- back/prisma/migrations/20260214132821_init_products/migration.sql
+++ back/prisma/migrations/20260214132821_init_products/migration.sql
@@ -2,15 +2,11 @@
 CREATE TABLE "Product" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
-    "slug" TEXT NOT NULL,
-    "description" TEXT NOT NULL,
-    "price" INTEGER NOT NULL,
+    "description" TEXT,
+    "price" DOUBLE PRECISION NOT NULL,
     "image" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
 );
-
--- CreateIndex
-CREATE UNIQUE INDEX "Product_slug_key" ON "Product"("slug");
