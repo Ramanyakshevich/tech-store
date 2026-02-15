98b9633b2753b6f8b884c1606cd98d4885dcfe13
RamanYakshevich <s227736@sggw.edu.pl>
2026-02-15 15:37:25 +0100

    final version of db

--- back/src/prisma/prisma.service.ts
+++ back/src/prisma/prisma.service.ts
@@ -1,13 +1,29 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
+import { Pool } from 'pg';
+import 'dotenv/config';
 
 @Injectable()
-export class PrismaService extends PrismaClient{
-    constructor() {
-        const adapter = new PrismaPg({
-            connectionString: process.env.DATABASE_URL,
-        })
-        super({ adapter });
-    }
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  constructor() {
+    const connectionString = process.env.DATABASE_URL;
+
+    const pool = new Pool({ 
+      connectionString,
+      ssl: connectionString?.includes('localhost') ? false : { rejectUnauthorized: false }
+    });
+
+    const adapter = new PrismaPg(pool);
+
+    super({ adapter });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+     await this.$disconnect();
+  }
 }
--- back/src/product/product.controller.ts
+++ back/src/product/product.controller.ts
@@ -1,7 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { ProductService } from './product.service';
 
 @Controller('product')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
+
+  @Post()
+  create(@Body() dto: any){
+    return this.productService.create(dto)
+  }
+
+  @Get()
+  findAll(){
+    return this.productService.findAll()
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string){
+    return this.productService.findOne(+id)
+  }
 }
